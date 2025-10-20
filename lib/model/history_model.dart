779ab8489fcb7b5b2b7dd846a988fa4c12f365e7
lenguyen-it <lethainguyenit2002@gmail.com>
2025-10-20 15:33:38 +0700

    xóa comment cũ

--- lib/model/history_model.dart
+++ lib/model/history_model.dart
@@ -1,39 +1,3 @@
-// class HistoryModel {
-//   final String idLocal;
-//   final String barcode;
-//   final int timestampDevice;
-//   final String status;
-//   final String? lastError;
-
-//   HistoryModel({
-//     required this.idLocal,
-//     required this.barcode,
-//     required this.timestampDevice,
-//     required this.status,
-//     this.lastError,
-//   });
-
-//   Map<String, dynamic> toMap() {
-//     return {
-//       'id_local': idLocal,
-//       'barcode': barcode,
-//       'timestamp_device': timestampDevice,
-//       'status': status,
-//       'last_error': lastError,
-//     };
-//   }
-
-//   factory HistoryModel.fromMap(Map<String, dynamic> map) {
-//     return HistoryModel(
-//       idLocal: map['id_local'],
-//       barcode: map['barcode'],
-//       timestampDevice: map['timestamp_device'],
-//       status: map['status'],
-//       lastError: map['last_error'],
-//     );
-//   }
-// }
-
 class HistoryModel {
   final String idLocal;
   final String epc;
